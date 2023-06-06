fded0c82f5ee30363cd297a961218a558eab32c6
Louise Barbarin <77415694+Hyziel@users.noreply.github.com>
2023-06-06 09:40:03 +0200

    Moiki storytelling TLE

---
+++ hooks.js
@@ -0,0 +1,16 @@
+/* UTILISEZ CE FICHIER POUR MODIFIER CERTAINES FONCTIONNALITÉS DU PLAYER */
+var story_hooks = {
+  sounds: {
+    /*
+    
+    // Exemple de modification des crédits d'un son
+
+    'fichier.mp3': {
+      credit: 'Texte de mon crédit',
+      license: 'CC',
+      linkName: 'Soundcloud',
+      link: 'https://soundcloud.com/sample'
+    }
+    */
+  }
+};
